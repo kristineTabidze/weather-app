747d778c4f965b0e5fe3185c1dd8f031ebdebfc0
Kristine Tabidze <kristine.tabidze@celfocus.com>
2022-11-24 23:16:30 +0400

    init project

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+
+
+/* eslint-disable global-require */
+
+module.exports = {
+    content: [
+        "./pages/**/*.{js,ts,jsx,tsx}",
+        "./components/**/*.{js,ts,jsx,tsx}",
+    ],
+    theme: {
+        extend: {},
+    },
+    plugins: [],
+};
